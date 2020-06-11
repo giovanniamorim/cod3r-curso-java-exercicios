24069d21247d566d012c4d97c3683a5d8c11a260
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2020-06-11 18:08:07 -0300

    12 - Primeiro Programa #02

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,10 @@
 package fundamentos;
 
 public class PrimeiroPrograma {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Primeiro Programa");
+		System.out.println("Primeiro Programa #01");
+		System.out.println("Promeiro Programa #02");
 	}
 
 }
